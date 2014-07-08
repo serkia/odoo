cb511cd663d020520ecb5cdf5bc1761b59f238a2
Jeremy Kersten <jke@odoo.com>
2014-07-08 11:53:28 +0200

    [IMP] crm campaign - get default value

--- addons/website_crm/ir_http.py
+++ addons/website_crm/ir_http.py
@@ -7,7 +7,7 @@ class ir_http(orm.AbstractModel):
     _inherit = 'ir.http'
 
     def _dispatch(self):
-        for var, dummy in self.pool['crm.tracking_mixin'].tracking_fields():
+        for var, dummy in self.pool['crm.tracking.mixin'].tracking_fields():
             if var in request.params and (var not in request.session or request.session[var] != request.params[var]):
                     request.session[var] = request.params[var]
         return super(ir_http, self)._dispatch()
