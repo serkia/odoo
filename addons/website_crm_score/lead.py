d0e95a7857851b64968c2004153a78dd0ae35b7d
ledebroux <leonard.debroux@gmail.com>
2014-08-05 10:08:50 +0200

    website crm scoring

---
+++ addons/website_crm_score/lead.py
@@ -0,0 +1,14 @@
+from openerp.osv import osv, fields
+
+
+class Lead(osv.Model):
+
+	_inherit = 'crm.lead'
+	
+	# _columns = {
+	# 	'score' : fields.integer("Score"),
+	# }
+
+	# _defaults = {
+	# 	'score' : 2,
+	# }
