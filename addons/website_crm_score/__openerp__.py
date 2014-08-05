0eae9e6984820a22a707009cdaaa40b5714cff8a
ledebroux <leonard.debroux@gmail.com>
2014-08-05 10:08:50 +0200

    website crm scoring

---
+++ addons/website_crm_score/__openerp__.py
@@ -0,0 +1,14 @@
+{
+	'name' : "Lead Scoring",
+	'category' : "Test",
+	'version' : "1.0",
+	'depends' : ['base', 'website_crm'],
+	'author' : "Me",
+	'description' : """\
+	Lead scoring""",
+	'data' : [
+		'views/lead.xml',
+		'views/template.xml',
+		'views/website_crm_score.xml',
+	],
+}
