05eb208d48632e81eabe7eae88e07029154def10
ledebroux <leonard.debroux@gmail.com>
2014-08-19 11:52:30 +0200

    before changing session_user

--- addons/website_crm_score/models/sales_team.py
+++ addons/website_crm_score/models/sales_team.py
@@ -3,6 +3,7 @@ from openerp import fields, api, models, SUPERUSER_ID
 import time
 import datetime
 from openerp.tools.safe_eval import safe_eval
+from random import randint
 
 AVAILABLE_RATIO = [
     ('0', 'No ratio'),
@@ -11,11 +12,6 @@ AVAILABLE_RATIO = [
     ('3', 'Normal'),
     ('4', 'High'),
     ('5', 'Very High'),
-]
-
-DOMAINS = [
-    ('ir.filters', 'Filters'),
-    ('res.partner', 'Customers'),
 ]
 
 
@@ -38,12 +34,29 @@ class crm_case_section(osv.osv):
 
         # Verifier/tester cette horreur
 
+        def get_filters(filter_ids):
+            filters = [safe_eval(f['domain']) for f in all_filters if f['id'] in filter_ids]
+            # flatten the list of filters
+            filters = [f for sublist in filters for f in sublist]
+            return filters
+
+        def list_to_dict_keys(d, l, v):
+            for e in l:
+                if e in d:
+                    d[e].append(v)
+                else:
+                    d[e] = [v]
+
+
         filters_fields = ['domain']
         all_filters = self.pool["ir.filters"].search_read(cr, SUPERUSER_ID, fields=filters_fields, context=context)
         print 'filters', all_filters
 
         salesteams_fields = ['filter_ids', 'leads_count', 'maximum_section_leads']
         all_salesteams = self.search_read(cr, SUPERUSER_ID, fields=salesteams_fields, context=context)
+        # casting the list into a dict to ease the access afterwards
+        all_salesteams = {team['id']: team for team in all_salesteams}
+        print 'all_salesteams', all_salesteams
 
         section_users_fields = ['section_domain', 'user_id', 'section_id', 'maximum_user_leads']
         all_section_users = self.pool['section.user'].search_read(cr, SUPERUSER_ID, fields=section_users_fields, context=context)
@@ -55,83 +68,97 @@ class crm_case_section(osv.osv):
         users_fields = ['leads_count']
         all_users = self.pool['res.users'].search_read(cr, SUPERUSER_ID, fields=users_fields, context=context)
 
-        for lead in all_leads:
-            if not lead['section_id'] and not lead['user_id']:
-                print lead
-                potential_salesteams = []
-                for salesteam in all_salesteams:
-                    filter_ids = salesteam['filter_ids']
-                    filters = [safe_eval(f['domain']) for f in all_filters if f['id'] in filter_ids]
-                    # flatten the list of filters
-                    filters = [f for sublist in filters for f in sublist]
-                    # should be done otherwise, it represents a lot of accesses to the db
-                    domain = [('id', '=', lead['id'])] + filters
-                    lead_fit = self.pool["crm.lead"].search(cr, SUPERUSER_ID, domain, context=context)
-                    if lead_fit:
-                        potential_salesteams.append(salesteam)
-                print 'potential_salesteams', potential_salesteams
-                # need to decide what to do with the lead, because if a team has no filters, it potentially gets everything
+        # lead assignement to salesteams
 
-                # self.pool["crm.lead"].write(cr, SUPERUSER_ID, lead['id'], {'section_id': chosen_team_id}, context=context)
-                # the chosen salesteams must be updated in the all_salesteam dict : lead_count += 1
-                # the lead should be updated in the dict : section_id = chosen_team_id
+        # V1
+        # for lead in all_leads:
+        #     if not lead['section_id'] and not lead['user_id']:
+        #         print lead
+        #         potential_salesteams = []
+        #         for salesteam in all_salesteams:
+        #             filter_ids = salesteam['filter_ids']
+        #             filters = get_filters(filter_ids)
+        #             # should be done otherwise, it represents a lot of accesses to the db
+        #             domain = [('id', '=', lead['id'])] + filters
+        #             lead_fit = self.pool["crm.lead"].search(cr, SUPERUSER_ID, domain, context=context)
+        #             if lead_fit:
+        #                 potential_salesteams.append(salesteam)
+        #         print 'potential_salesteams', potential_salesteams
+        #         # need to decide what to do with the lead, because if a team has no filters, it potentially gets everything
 
-        for salesteam in all_salesteams:
-            domain = [('section_id', '=', salesteam['id'])]
-            team_leads = [lead for lead in all_leads if lead['section_id'] == salesteam['id']]
+        #         # self.pool["crm.lead"].write(cr, SUPERUSER_ID, lead['id'], {'section_id': chosen_team_id}, context=context)
+        #         # the chosen salesteams must be updated in the all_salesteam dict : lead_count += 1
+        #         # the lead should be updated in the dict : section_id = chosen_team_id
+
+        # V2
+        potential_salesteams_for_leads = {}
+        for _, salesteam in all_salesteams.iteritems():
+            filter_ids = salesteam['filter_ids']
+            filters = get_filters(filter_ids)
+            # should be done otherwise, it represents a lot of accesses to the db
+            domain = [('section_id', '=', False), ('user_id', '=', False)] + filters
+            lead_fit = self.pool["crm.lead"].search(cr, SUPERUSER_ID, domain, context=context)
+            if lead_fit:
+                print 'salesteam', salesteam
+                list_to_dict_keys(potential_salesteams_for_leads, lead_fit, salesteam['id'])
+        print 'potential_salesteams_for_leads', potential_salesteams_for_leads
+
+        # /!\ section_id contains a tuple
+        dummyString = 'dummy'
+        for lead_id, salesteams_ids in potential_salesteams_for_leads.iteritems():
+            lead = [lead for lead in all_leads if lead['id'] == lead_id][0]
+            print 'lead', lead
+            while not lead['section_id'] and salesteams_ids:
+                # electing a salesteam to get the lead
+                r = randint(0, len(salesteams_ids) - 1)
+                salesteam_id = salesteams_ids[r]
+                salesteam = all_salesteams[salesteam_id]
+                if salesteam['leads_count'] < salesteam['maximum_section_leads']:
+                    # todo: write in db here
+                    # updates value in all_leads and all_salesteams
+                    lead['section_id'] = (salesteam['id'], dummyString)
+                    salesteam['leads_count'] += 1
+                else:
+                    del salesteams_ids[r]
+            print 'lead_after', lead
+
+
+
+
+
+
+        # lead assignement to salesmen
+
+        for _, salesteam in all_salesteams.iteritems():
+            # domain = [('section_id', '=', salesteam['id'])]
+            # todo: this [0] is not so nice, should I do otherwise ?
+            team_leads = [lead for lead in all_leads if lead['section_id'] and lead['section_id'][0] == salesteam['id'] and not lead['user_id']]
             # need to decide which leads will be asigned, I first consider that's all of them
+            to_assign_leads = team_leads
+            # print 'to_assign_leads', to_assign_leads
+            to_assign_leads_ids = [lead['id'] for lead in to_assign_leads]
+            # print to_assign_leads_ids
 
+            potential_salesmen_for_leads = {}
+            for section_user in all_section_users:
+                # print 'section_user'#, section_user
+                # todo: this [0] is not so nice, should I do otherwise ?
+                if section_user['section_id'][0] == salesteam['id']:
+                    # get the filters of this salesman
+                    section_user_domain = section_user['section_domain']
+                    filter_ids = safe_eval(section_user_domain)[0][2]
+                    filters = get_filters(filter_ids)
+                    # print filters
+                    # should be done otherwise, it represents a lot of accesses to the db
+                    domain = [('id', 'in', to_assign_leads_ids)] + filters
+                    lead_fit = self.pool["crm.lead"].search(cr, SUPERUSER_ID, domain, context=context)
+                    # print "lead_fit", lead_fit
+                    if lead_fit:
+                        list_to_dict_keys(potential_salesmen_for_leads, lead_fit, section_user)
+            print 'dict', potential_salesmen_for_leads
 
-
-        # print "for1"
-        # for team in self.search_read(cr, uid, fields=['filter_ids', 'leads_count', 'maximum_section_leads'], context=context):
-        #     print 'team', team
-        #     domain = team['filter_ids']
-        #     # filters associated with the current team
-        #     filters = self.pool["ir.filters"].search_read(cr, uid, domain=[('id', 'in', domain)], fields=['domain'], context=context)
-        #     # making sure that the lead is not yet assigned
-        #     print 'filters', filters
-        #     domain = [('section_id', '=', False), ('user_id', '=', False)]
-        #     for f in filters:
-        #         domain.extend(safe_eval(f['domain']))
-        #     print 'domain', domain
-        #     potential_leads = self.pool["crm.lead"].search(cr, uid, domain, context=context)
-        #     print potential_leads
-        #     for lead_id in potential_leads:
-        #         # assigning all potential_leads to the salesteam
-        #         # todo: are the domains exclusive ?
-        #         if team['leads_count'] < team['maximum_section_leads']:
-        #             self.pool["crm.lead"].write(cr, SUPERUSER_ID, lead_id, {'section_id': team['id']}, context=context)
-
-        #     print "for2"
-        #     for man in self.pool['section.user'].search_read(cr, SUPERUSER_ID, [('section_id', '=', team['id'])], fields=['section_domain', 'user_id', 'maximum_user_leads'], context=context):
-        #         print 'man', man
-        #         section_domain = man['section_domain']
-        #         if section_domain:
-        #             section_domain = safe_eval(man['section_domain'])
-        #             filters = self.pool["ir.filters"].search_read(cr, SUPERUSER_ID, section_domain, fields=['domain'], context=context)
-        #         else:
-        #             filters = []
-
-        #         # print 'section_domain', section_domain
-        #         # filters = self.pool["ir.filters"].search_read(cr, SUPERUSER_ID, section_domain, fields=['domain'], context=context)
-
-        #         # getting the filters on the salesman
-        #         # filters = self.pool["ir.filters"].search_read(cr, SUPERUSER_ID, domain, fields= ['domain'], context=context)
-        #         print 'filters', filters
-        #         # making sure that the leads are in unassigned to a salesman and for this salesteam
-        #         domain = [('section_id', '=', team['id']), ('user_id', '=', False)]
-        #         for f in filters:
-        #             domain.extend(safe_eval(f['domain']))
-        #         print 'domain', domain
-        #         potential_leads = self.pool["crm.lead"].search(cr, uid, domain, context=context)
-        #         print potential_leads
-        #         for lead_id in potential_leads:
-        #             user = self.pool['res.users'].search_read(cr, SUPERUSER_ID, [('id', '=', man['user_id'][0])], fields=['leads_count'], context=context)
-        #             print 'user', user
-        #             if user[0]['leads_count'] < man['maximum_user_leads']:
-        #                 self.pool["crm.lead"].write(cr, SUPERUSER_ID, lead_id, {'user_id': man['id']}, context=context)
-
+            # leads in potential_salesmen_for_leads must be assigned how should if be done ?
+            # write in the db the user_id, update the lead count on the users
 
 class res_users(osv.Model):
     _inherit = 'res.users'
@@ -159,6 +186,7 @@ class res_users(osv.Model):
 class section_user(models.Model):
 
     _name = 'section.user'
+    
 
     @api.one
     def _get_percentage(self):
