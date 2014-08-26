cf138dec55702d8fdf120cbab8024b91c1410167
ledebroux <leonard.debroux@gmail.com>
2014-08-26 16:16:14 +0200

    dispatch and js removed

--- addons/website_crm_score/static/src/js/set_score.js
+++ addons/website_crm_score/static/src/js/set_score.js
@@ -10,56 +10,56 @@
     // website.score = {};
 
 
-    // website.seo.Configurator.include({ 
-    //     track: null,
-    //     start: function() {
-    //         this._super.apply(this, arguments);
-    //         var self = this;
-    //         this.is_tracked().then(function(data){
-    //             if (data[0]['track']) {
-    //                 var $add = $('<input type="checkbox" checked="checked" required="required"/>');
-    //                 self.track = true;
-    //             }
-    //             else {
-    //                 var $add = $('<input type="checkbox" required="required"/>');
-    //                 self.track = false;
-    //             }
-    //             self.$el.find('h3[class="track-page"]').append($add); 
+    website.seo.Configurator.include({ 
+        track: null,
+        start: function() {
+            this._super.apply(this, arguments);
+            var self = this;
+            this.is_tracked().then(function(data){
+                if (data[0]['track']) {
+                    var $add = $('<input type="checkbox" checked="checked" required="required"/>');
+                    self.track = true;
+                }
+                else {
+                    var $add = $('<input type="checkbox" required="required"/>');
+                    self.track = false;
+                }
+                self.$el.find('h3[class="track-page"]').append($add); 
 
-    //         });
-    //     },
-    //     is_tracked: function(val) {
-    //         var obj = website.seo.Configurator.prototype.getMainObject();
-    //         if (!obj) {
-    //             return $.Deferred().reject();
-    //         } else {
-    //             return website.session.model(obj.model).call('read', [[obj.id], ['track'], website.get_context()]);
-    //         }
-    //     },
-    //     update: function () {
-    //         var self = this;
-    //         var mysuper = this._super;
-    //         var checkbox_value = this.$el.find('input[type="checkbox"]').is(':checked')
-    //         if (checkbox_value != self.track) {
-    //             this.trackPage(checkbox_value).then(function() {
-    //                 mysuper.call(self);
-    //             });
-    //         }
-    //         else {
-    //             mysuper.call(self);
-    //         }
+            });
+        },
+        is_tracked: function(val) {
+            var obj = website.seo.Configurator.prototype.getMainObject();
+            if (!obj) {
+                return $.Deferred().reject();
+            } else {
+                return website.session.model(obj.model).call('read', [[obj.id], ['track'], website.get_context()]);
+            }
+        },
+        update: function () {
+            var self = this;
+            var mysuper = this._super;
+            var checkbox_value = this.$el.find('input[type="checkbox"]').is(':checked')
+            if (checkbox_value != self.track) {
+                this.trackPage(checkbox_value).then(function() {
+                    mysuper.call(self);
+                });
+            }
+            else {
+                mysuper.call(self);
+            }
             
-    //     },
-    //     trackPage: function(val) {
-    //         var obj = website.seo.Configurator.prototype.getMainObject();
-    //         if (!obj) {
-    //             return $.Deferred().reject();
-    //         } else {
-    //             return website.session.model(obj.model).call('write', [[obj.id], { track: val }, website.get_context()]);
-    //         }
-    //     },
+        },
+        trackPage: function(val) {
+            var obj = website.seo.Configurator.prototype.getMainObject();
+            if (!obj) {
+                return $.Deferred().reject();
+            } else {
+                return website.session.model(obj.model).call('write', [[obj.id], { track: val }, website.get_context()]);
+            }
+        },
 
-    // });
+    });
 
     // website.score.Creator = openerp.Widget.extend({
     //     template: 'website.create.score',
