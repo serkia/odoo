f1a987cbb32ca327e1f938fa0061ec6bb9e0e7a1
Mitesh Savani <msa@openerp.com>
2014-08-27 12:24:06 +0530

    [IMP]hr_recruitment:change stages of job positions as per department wise to job wise.

--- addons/hr_recruitment/hr_recruitment.py
+++ addons/hr_recruitment/hr_recruitment.py
@@ -51,15 +51,22 @@ class hr_recruitment_stage(osv.osv):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
         'sequence': fields.integer('Sequence', help="Gives the sequence order when displaying a list of stages."),
-        'department_id':fields.many2one('hr.department', 'Specific to a Department', help="Stages of the recruitment process may be different per department. If this stage is common to all departments, keep this field empty."),
+        'job_ids': fields.many2many('hr.job', 'job_stage_rel', 'stage_id', 'job_id', 'Job Stages'),
         'requirements': fields.text('Requirements'),
         'template_id': fields.many2one('email.template', 'Use template', help="If set, a message is posted on the applicant using the template when the applicant is set to the stage."),
         'fold': fields.boolean('Folded in Kanban View',
                                help='This stage is folded in the kanban view when'
                                'there are no records in that stage to display.'),
     }
+
+    def _get_default_job_ids(self, cr, uid, context={}):
+        if context.get('default_job_id'):
+            return [context.get('default_job_id')]
+        return None
+
     _defaults = {
         'sequence': 1,
+        'job_ids': _get_default_job_ids,
     }
 
 class hr_recruitment_degree(osv.osv):
@@ -101,8 +108,7 @@ class hr_applicant(osv.Model):
 
     def _get_default_stage_id(self, cr, uid, context=None):
         """ Gives default stage_id """
-        department_id = self._get_default_department_id(cr, uid, context=context)
-        return self.stage_find(cr, uid, [], department_id, [('fold', '=', False)], context=context)
+        return self.stage_find(cr, uid, [], context.get('default_job_id'), [('fold', '=', False)], context=context)
 
     def _resolve_department_id_from_context(self, cr, uid, context=None):
         """ Returns ID of department based on the value of 'default_department_id'
@@ -140,11 +146,11 @@ class hr_applicant(osv.Model):
         # - ('id', 'in', 'ids'): add columns that should be present
         # - OR ('department_id', '=', False), ('fold', '=', False): add default columns that are not folded
         # - OR ('department_id', 'in', department_id), ('fold', '=', False) if department_id: add department columns that are not folded
-        department_id = self._resolve_department_id_from_context(cr, uid, context=context)
+        job_id = context.get('default_job_id')
         search_domain = []
-        if department_id:
-            search_domain += ['|', ('department_id', '=', department_id)]
-        search_domain += ['|', ('id', 'in', ids), ('department_id', '=', False)]
+        if job_id:
+            search_domain += ['|', ('job_ids', '=', job_id)]
+        search_domain += [('id', 'in', ids)]
         stage_ids = stage_obj._search(cr, uid, search_domain, order=order, access_rights_uid=access_rights_uid, context=context)
         result = stage_obj.name_get(cr, access_rights_uid, stage_ids, context=context)
         # restore order of the search
@@ -195,7 +201,7 @@ class hr_applicant(osv.Model):
         'create_date': fields.datetime('Creation Date', readonly=True, select=True),
         'write_date': fields.datetime('Update Date', readonly=True),
         'stage_id': fields.many2one ('hr.recruitment.stage', 'Stage', track_visibility='onchange',
-                        domain="['|', ('department_id', '=', department_id), ('department_id', '=', False)]"),
+                        domain="[('job_ids', '=', job_id)]", copy=False),
         'last_stage_id': fields.many2one('hr.recruitment.stage', 'Last Stage',
                                          help='Stage of the applicant before being in the current stage. Used for lost cases analysis.'),
         'categ_ids': fields.many2many('hr.applicant_category', string='Tags'),
@@ -249,19 +255,18 @@ class hr_applicant(osv.Model):
         'stage_id': _read_group_stage_ids
     }
 
-    def onchange_job(self, cr, uid, ids, job_id=False, context=None):
+    def onchange_job(self, cr, uid, ids, job_id=False, stage_id=False, context=None):
         department_id = False
         user_id = False
         if job_id:
             job_record = self.pool.get('hr.job').browse(cr, uid, job_id, context=context)
             department_id = job_record and job_record.department_id and job_record.department_id.id or False
             user_id = job_record and job_record.user_id and job_record.user_id.id or False
-        return {'value': {'department_id': department_id, 'user_id': user_id}}
-
-    def onchange_department_id(self, cr, uid, ids, department_id=False, stage_id=False, context=None):
-        if not stage_id:
-            stage_id = self.stage_find(cr, uid, [], department_id, [('fold', '=', False)], context=context)
-        return {'value': {'stage_id': stage_id}}
+        if stage_id:
+            return {'value': {'department_id': department_id, 'user_id': user_id, 'stage_id':stage_id}}
+        else:
+            stage_id = self.stage_find(cr, uid, [], job_id, [('fold', '=', False)], context=context)
+        return {'value': {'department_id': department_id, 'user_id': user_id, 'stage_id':stage_id}}
 
     def onchange_partner_id(self, cr, uid, ids, partner_id, context=None):
         data = {'partner_phone': False,
@@ -282,30 +287,25 @@ class hr_applicant(osv.Model):
             return {'value': {'date_closed': fields.datetime.now()}}
         return {'value': {'date_closed': False}}
 
-    def stage_find(self, cr, uid, cases, section_id, domain=[], order='sequence', context=None):
+    def stage_find(self, cr, uid, applicants, job_id, domain=[], order='sequence', context=None):
         """ Override of the base.stage method
             Parameter of the stage search taken from the lead:
             - department_id: if set, stages must belong to this section or
               be a default case
         """
-        if isinstance(cases, (int, long)):
-            cases = self.browse(cr, uid, cases, context=context)
-        # collect all section_ids
-        department_ids = []
-        if section_id:
-            department_ids.append(section_id)
-        for case in cases:
-            if case.department_id:
-                department_ids.append(case.department_id.id)
-        # OR all section_ids and OR with case_default
+        if isinstance(applicants, (int, long)):
+            applicants = self.browse(cr, uid, applicants, context=context)
+        job_ids = set([applicant.job_id.id for applicant in applicants if applicant.job_id])
+        if job_id:
+            job_ids.add(job_id)
+        # OR all job_id and OR with applicant_default
         search_domain = []
-        if department_ids:
-            search_domain += ['|', ('department_id', 'in', department_ids)]
-        search_domain.append(('department_id', '=', False))
+        if job_ids:
+            search_domain = [('job_ids', 'in', list(job_ids))]
         # AND with the domain in parameter
         search_domain += list(domain)
         # perform search, return the first found
-        stage_ids = self.pool.get('hr.recruitment.stage').search(cr, uid, search_domain, order=order, context=context)
+        stage_ids = self.pool.get('hr.recruitment.stage').search(cr, uid, search_domain, order=order, limit=1, context=context)
         if stage_ids:
             return stage_ids[0]
         return False
@@ -410,7 +410,8 @@ class hr_applicant(osv.Model):
             context['default_department_id'] = vals.get('department_id')
         if vals.get('job_id') or context.get('default_job_id'):
             job_id = vals.get('job_id') or context.get('default_job_id')
-            vals.update(self.onchange_job(cr, uid, [], job_id, context=context)['value'])
+            stage_id = vals.get('stage_id')
+            vals.update(self.onchange_job(cr, uid, [], job_id, stage_id, context=context)['value'])
         if vals.get('user_id'):
             vals['date_open'] = fields.datetime.now()
         if 'stage_id' in vals:
@@ -553,6 +554,7 @@ class hr_job(osv.osv):
         }
 
     _columns = {
+        'stage_ids': fields.many2many('hr.recruitment.stage','job_stage_rel','job_id','stage_id','Job Stages'),
         'survey_id': fields.many2one('survey.survey', 'Interview Form', help="Choose an interview form for this job position and you will be able to print/answer this interview from all applicants who apply for this job"),
         'alias_id': fields.many2one('mail.alias', 'Alias', ondelete="restrict", required=True,
                                     help="Email alias for this job position. New emails will automatically "
