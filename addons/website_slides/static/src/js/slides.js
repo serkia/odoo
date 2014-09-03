0fe2049470be970e5f459ba19512730edb796fd8
Vijay Patel <vip@openerp.com>
2014-09-03 14:26:26 +0530

    [imp] website_slides: implimented cookies in like unlike buttons in slide detail view
    [imp] website_slides: optimized like deslike code for localstorage in slide detail view

--- addons/website_slides/static/src/js/slides.js
+++ addons/website_slides/static/src/js/slides.js
@@ -332,12 +332,15 @@ $(document).ready(function() {
 
     $('.slide-like, .slide-unlike').on('click', function(ev){
         ev.preventDefault();
-        if(!localStorage['vote']){
+        var link_id = $(this).attr('id');
+        var attachment_id = $(this).attr('attachment-id');
+        var user_id = $(this).attr('user-id');
+        if(localStorage[link_id+'_'+attachment_id] != user_id){
             var $link = $(ev.currentTarget);
             openerp.jsonRpc($link.data('href'), 'call', {}).then(function(data){
                     $($link.data('count-el')).text(data);
             });
-            localStorage['vote'] = true;
+            localStorage[link_id+'_'+attachment_id] = user_id;
         }
     });
     $('.upload').on('click' ,function(ev){
