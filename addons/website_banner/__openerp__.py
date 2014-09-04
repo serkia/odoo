4aaccb5e200943fe5f6a0492bdb1478f2a9a99ca
Ajay javiya <aja@openerp.com>
2014-09-04 17:16:31 +0530

    [ADD] : website_banner module

---
+++ addons/website_banner/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Website Banner',
+    'description': 'Website Banner',
+    'category': 'Theme',
+    'version': '1.0',
+    'author': 'OpenERP SA',
+    'depends': ['website'],
+    'data': [
+        'views/website_banner.xml',
+    ],
+    'application': True,
+}
